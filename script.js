dede7ad51259008204300b790ac6620dcd42c411
monkeycs60 <clement.serizay@gmail.com>
2022-04-04 22:39:15 +0200

    soluce algo 20

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-var convertisseurRomain = function(num) {
-
-    var valeurDecimale = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
-    var chiffreRomains = 
-    [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
-
-
+function additioneNbPremiers(num) {
+    
+let decomposeNbr = [];
+let decomposeOnlyPremier = [];
+for (let index = num; index > 1; index--) {
+    decomposeNbr.push(index);
 }
-      
-console.log(convertisseurRomain(36));
+for (let j = 0; j < decomposeNbr.length; j++) {
+    if (decomposeNbr[j] % 2 > 0 && decomposeNbr[j] % 3 > 0 && decomposeNbr[j] % 5 > 0 && decomposeNbr[j] % 7 > 0)  {
+        decomposeOnlyPremier.push(decomposeNbr[j]);
+    }
+    else if ( decomposeNbr[j] === 2 || decomposeNbr[j] === 3 || decomposeNbr[j] === 5 || decomposeNbr[j] === 7) {
+        decomposeOnlyPremier.push(decomposeNbr[j]);
+    }
+}
+console.log(decomposeOnlyPremier);
+let result = 0;
+for (let k = 0; k < decomposeOnlyPremier.length; k++) {
+    result += decomposeOnlyPremier[k]
+    
+}
+return result
+}
 
-// 36 = XXXVI
-// 2000 = MM
-// 5648 = MMMMMDCXLVIII
+console.log(additioneNbPremiers(10));
+
+// additioneNbPremiers(10) = 17;
 
 // ----------------- ÉNONCÉ -----------------
 
-// Vous devez créer un algorithme qui va convertir des le nombre passé en parametre
-// en chiffre Romain !
-// Vous disposez de deux tableaux pour faire les conversions.
+// Additione tous les nombres premiers d'un nombre donné, en comptant ce nombre.
+// Les nombres premiers sont les nombres qui peuvent seulement être divisés par 1 et
+// par eux-mêmes.
+// 1 n'est pas un nombre premier.
+// La liste des nombres premiers commencent comme cela : 
+// 2, 3, 5, 7, 11, 13, 17, 19 etc ...
 
 // ----------------- CONSEILS -----------------
 
-// For;
-// while;
+// Recursion.
+// Fonction dans une fonction.
+// If, For ...
+// Modulo (%).
