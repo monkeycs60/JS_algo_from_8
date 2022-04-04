f67ea2d6a5833d46647b86b12a95b08aabcac054
monkeycs60 <clement.serizay@gmail.com>
2022-04-05 00:57:17 +0200

    algo 3

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-function abreviation(str) {
-    let nomInit = [];
-    let result = str.trim();
-    let espace = result.indexOf(" ");
-    let prenom = result.slice(0, espace);
+function anagramme(strA, strB) {
+    let regexp = /[^$*^ ,]+/g;
+    let firstTabA = strA.match(regexp);
+    let firstTabB = strB.match(regexp);
+    let makeTabA = firstTabA.join('').toString().toLowerCase().split("");
+    let makeTabB = firstTabB.join('').toString().toLowerCase().split("");
     
-for (let index = 0; index < result.length; index++) {
-    let regexp = /[a-zA-Z]+/g;
-    if (result[index] === " ") {
-        nomInit.push(result[index + 1]);
-    }   
-}
-let last = `${prenom} ${nomInit}.`;
-return last
-};
+    let check = 0;
+    for (let index = 0; index < makeTabA.length; index++) {
+        for (let k = 0; k < makeTabB.length; k++) {
+            if (makeTabA[index] == makeTabB[k]) {
+                check += 1;
+            }
+        }
+    }
+  console.log(check);
+        if (check == makeTabB.length) {
+            return true;
+        }
+        else {
+            return false;
+        }
+  }
 
-console.log(abreviation("John Doe")); // John D.
-console.log(abreviation("Romy Schneider")); // Romy S.
-console.log(abreviation("             Alfred Hitchcock                 ")); // Alfred H.
-console.log(abreviation("Jean-Michel Luc"));
+
+
+
+console.log(anagramme("Faible", " fiable")); // True
+console.log(anagramme("$*^^gare", "        Rag    e")); // True
+console.log(anagramme("bougie", "piano")); // False
+console.log(anagramme("faible", "fiable"));
+
+
+
 
 
 
 // -----------------  Énoncé ------------------- //
 
-// Créez un algorithme qui va retourner l'abréviation d'un nom et prénom, qui va retourner "John D." quand on lui passe en argument "John Doe"
+// Créez un algorithme qui va retourner true si le premier paramètre est un anagramme du second, false dans le cas contraire.
+// Malheuresement les entrées contiennent parfois des symboles et des espaces qu'il va falloir supprimer des chaînes.
+
 
 // -----------------  Conseils ------------------- //
