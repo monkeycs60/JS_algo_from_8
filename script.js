92937a2f8a304f7c38964d9b0aa28a72eae97b22
monkeycs60 <clement.serizay@gmail.com>
2022-04-04 19:34:33 +0200

    ALGO 19 DEBUT

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-function telephoneCheck(str){
-    
-    var reg = /(0|\+33)[1-9][.\- ][0-9]{2}[.\- ][0-9]{2}[.\- ][0-9]{2}[.\- ][0-9]{2}/mg;
+var convertisseurRomain = function(num) {
 
-    return reg.test(str);
+    var valeurDecimale = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var chiffreRomains = 
+    [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
 
 }
+      
+console.log(convertisseurRomain(36));
 
-console.log(telephoneCheck("06 44 64 90 21"));
-
+// 36 = XXXVI
+// 2000 = MM
+// 5648 = MMMMMDCXLVIII
 
 // ----------------- ÉNONCÉ -----------------
 
-// Vous devez vous assurer que vos utilisateurs rentrent bien un numéro Français.
-// Ce numéro doit : 
-// Démarrer par un 0 et être suivi d'un chiffre entre 1-9.
-// Il faut qu'il y est 4 duos de chiffre de 1 à 9 après les deux premiers.
-// Nous accepterons des espaces( ), tirets(-) et points(.) entre les duos de
-// chiffres.
-// Retournez true si la chaine en parametre passe le test(si c'est un numéro valable)
-// et false en cas contraire.
-
+// Vous devez créer un algorithme qui va convertir des le nombre passé en parametre
+// en chiffre Romain !
+// Vous disposez de deux tableaux pour faire les conversions.
 
 // ----------------- CONSEILS -----------------
 
-// Regex time!
-// Google est votre ami.
-// "Regex tool online" aussi.
-// La document MDN et autre.
-
-// Bon courage.
+// For;
+// while;
